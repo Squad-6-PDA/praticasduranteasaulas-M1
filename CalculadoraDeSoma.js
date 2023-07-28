da6501e4989a73a14de4bd3c53450c7717a70f4e
Janine Cruz <137451313+JanineCruz921@users.noreply.github.com>
2023-07-27 21:30:47 -0300

    Add files via upload
    Subindo atividade calculadora de soma

---
+++ CalculadoraDeSoma.js
@@ -0,0 +1,8 @@
+var numero1 = prompt ("Digite um número")
+var numero2 = prompt ("Digite um segundo número")
+
+function soma (a, b){
+  return Number (a) + Number(b)
+}
+
+alert ("O total da soma é " + soma(numero1, numero2))
