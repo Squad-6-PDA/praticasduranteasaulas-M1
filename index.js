3ba1a4fce59cc19539cbc778cebd0b46cc481771
Oziel Sousa <ozielzin358@gmail.com>
2023-08-03 18:32:15 -0300

    first commit <3

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-var valor1 = parseInt(prompt("Digite um VALOR"));
-var operacaoUsuario = prompt("Digite a OPERAÇÃO de deseja, ex: +, -, / ou * ");
-var valor2 = parseInt(prompt("Digite o SEGUNDO VALOR"));
 
-function calculadora(a, operacao, b) {
-  if (operacao == "+" ) {
-    return soma(a, b);
-  } else if (operacao == "-") {
-    return subtracao(a, b);
-  } else if (operacao == "/") {
-    return divisao(a, b);
-  } else if (operacao == "*") {
-    return multiplicacao(a, b);
-  }else{
-    alert("Deu Errado!")
-  }
-}
-
-function soma(a, b) {
-  return a + b;
-}
-
-function subtracao(a, b) {
-  return a - b;
-}
-
-function multiplicacao(a, b) {
-  return a * b;
-}
-
-function divisao(a, b) {
-  return a / b;
-}
-
-function exibirResultado(a, operacao, b) {
-    alert("Seu resultado é: " + (calculadora(valor1, operacaoUsuario, valor2)))
-}
-
-
-function calcular(a, operacao, b){
-    exibirResultado();
+for(var valor=0; valor < 11; valor++){
+    document.write("Número " + valor)
 }
